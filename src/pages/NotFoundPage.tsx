b72e70020ef4e35ca6c78db780f23b375a25b61c
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-11-25 19:38:56 +0200

    404 page styling

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from '@emotion/styled'
+
+const StyledLink = styled(Link)(() => ({
+  color: 'white',
+  textDecoration: 'none',
+  fontSize: '2rem',
+  '&:hover': {
+    color: 'white',
+    textDecoration: 'underline white'
+  }
+}))
 
 export const NotFoundPage = () => {
   return (
     <React.Fragment>
-      <div>NotFoundPage</div>
-      <Link to='/'>Go to Home Page</Link>
+      <div className='page-404-container'>
+        <h1>Page Not Found</h1>
+        <StyledLink to='/'>Go to Home Page</StyledLink>
+      </div>
     </React.Fragment>
   )
 }
