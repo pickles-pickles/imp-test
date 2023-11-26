8d9a47b83b192036cdce4c1767d9dde75334ff2a
pickles-pickles <75488028+pickles-pickles@users.noreply.github.com>
2023-11-26 13:58:20 +0200

    updated progress bar UI

--- src/components/tables/ProgressBar.tsx
+++ src/components/tables/ProgressBar.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
 import LinearProgress from '@mui/material/LinearProgress'
+import { TableCell, TableRow } from '@mui/material'
 
 export const ProgressBar = () => {
   return (
-    <React.Fragment>
-      <LinearProgress
-        color='secondary'
-        sx={{ minHeight: 25, background: '#05EE53 ' }}
-      />
-      <LinearProgress
-        color='success'
-        sx={{ minHeight: 25, background: '#0E12F7' }}
-      />
-      <LinearProgress sx={{ minHeight: 25, background: '#9400FF' }} />
-    </React.Fragment>
+    <TableRow>
+      <TableCell colSpan={9}>
+        <LinearProgress
+          color='secondary'
+          sx={{ minHeight: 25, background: '#05EE53 ' }}
+        />
+        <LinearProgress
+          color='success'
+          sx={{ minHeight: 25, background: '#0E12F7' }}
+        />
+        <LinearProgress sx={{ minHeight: 25, background: '#9400FF' }} />
+      </TableCell>
+    </TableRow>
   )
 }
